1d1c627ffd6b3be8fb90ad9c441133f9d66d572c
Arturo Gonzalez Breton <arturo.gbreton@gmail.com>
2022-11-28 21:10:39 -0600

    Add models to scinet/admin.py

--- scinet_app/admin.py
+++ scinet_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Institution
+from .models import Generaluser
+from .models import Journal
+from .models import Publication
+from .models import Writes
+from .models import Belongs
 
-# Register your models here.
+admin.site.register(Institution)
+admin.site.register(Generaluser)
+admin.site.register(Journal)
+admin.site.register(Publication)
+admin.site.register(Writes)
+admin.site.register(Belongs)
