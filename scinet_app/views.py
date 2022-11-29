63c1fa9aaa81581f848cce6a462bfb331ac2e76e
Mario Andrés Rosales Peña <mariorosales@ciencias.unam.mx>
2022-11-29 17:35:27 -0600

    frontEnd

--- scinet_app/views.py
+++ scinet_app/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context
 
+def vista(request):
+	archivo = open('scinet_app/templates/index.html')
+	plantilla = Template(archivo.read())
+	archivo.close()
+	contexto = Context()
+	html = plantilla.render(contexto)
+	return HttpTesponse(html)
