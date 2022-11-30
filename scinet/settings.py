29d48cf80e3a177c8a77d1c10158738286631f10
Mario Andrés Rosales Peña <mariorosales@ciencias.unam.mx>
2022-11-30 05:01:06 -0600

    Change HttpResponse to render

--- scinet/settings.py
+++ scinet/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'scinet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['scinet_app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- scinet_app/views.py
+++ scinet_app/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Template, Context
 
 def index(request):
-	archivo = open('scinet_app/templates/index.html')
-	plantilla = Template(archivo.read())
-	archivo.close()
-	contexto = Context()
-	html = plantilla.render(contexto)
-	return HttpResponse(html)
+	return render(request, 'index.html')
