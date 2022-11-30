e3a4bd9470ab9e92d842838ba57c8c1e89e63c3a
Mario Andrés Rosales Peña <mariorosales@ciencias.unam.mx>
2022-11-29 18:14:06 -0600

    Add index.html to scinet_app

--- scinet_app/views.py
+++ scinet_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template, Context
 
-def vista(request):
+def index(request):
 	archivo = open('scinet_app/templates/index.html')
 	plantilla = Template(archivo.read())
 	archivo.close()
 	contexto = Context()
 	html = plantilla.render(contexto)
-	return HttpTesponse(html)
+	return HttpResponse(html)
