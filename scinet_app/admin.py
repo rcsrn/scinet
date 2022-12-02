757fb29d67cf2a429317502f8122cf938f946d0b
Arturo Gonzalez Breton <arturo.gbreton@gmail.com>
2022-12-02 16:59:43 -0600

    Add resources.py

--- scinet_app/admin.py
+++ scinet_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Institution
 from .models import Generaluser
 from .models import Journal
@@ -6,82 +7,34 @@ from .models import Publication
 from .models import Writes
 from .models import Belongs
 from .models import Quotes
-from import_export import resources
+from .resources import *
 from import_export.admin import ImportExportModelAdmin
-
-class InstitutionResource(resources.ModelResource):
-    class Meta: 
-        model = Institution
-        skip_unchanged = True
-        report_skipped = True
-        import_id_fields = ('id')
-        exclude = ('id')
-        fields = ('id', 'institution_id', 'name')
         
 class InstitutionAdmin(ImportExportModelAdmin):
     resource_class = InstitutionResource
-    
-class GeneraluserResource(resources.ModelResource):
-    class Meta: 
-        model = Generaluser
-        skip_unchanged = True
-        report_skipped = True
-        import_id_fields = ('id')
-        exclude = ('id')
-        fields = ('id', 'general_user_id', 'username','first_name',
-                  'last_name','email','age','is_alive','is_author')
+    list_display = ['institution_id', 'name']
+
         
 class GenerlaluserAdmin(ImportExportModelAdmin):
     resource_class = GeneraluserResource
-    
-class BelongsResource(resources.ModelResource):
-    class Meta: 
-        model = Belongs
-        skip_unchanged = True
-        report_skipped = True
-        import_id_fields = ('id')
-        exclude = ('id')
-        fields = ('id', 'institution_id', 'general_useer_id')
+    list_display = ["general_user_id", "username", "first_name", "last_name",
+                    "email", "age", "is_alive", "is_author"]
         
 class BelongsAdmin(ImportExportModelAdmin):
     resource_class = BelongsResource
-    
-class JournalResource(resources.ModelResource):
-    class Meta: 
-        model = Journal
-        skip_unchanged = True
-        report_skipped = True
-        import_id_fields = ('id')
-        exclude = ('id')
-        fields = ('id', 'journal_id', 'name')
+    list_display = ["institution_id", "general_user_id"]
         
 class JournalAdmin(ImportExportModelAdmin):
     resource_class = JournalResource
-    
-class PublicationResource(resources.ModelResource):
-    class Meta: 
-        model = Publication
-        skip_unchanged = True
-        report_skipped = True
-        import_id_fields = ('id')
-        exclude = ('id')
-        fields = ('id', 'publication_id', 'journal_id', 'title',
-                  'publication_date')
+    list_display = ["journal_id", "name"]
         
 class PublicationAdmin(ImportExportModelAdmin):
     resource_class = PublicationResource
-    
-class QuotesResource(resources.ModelResource):
-    class Meta: 
-        model = Quotes
-        skip_unchanged = True
-        report_skipped = True
-        import_id_fields = ('id')
-        exclude = ('id')
-        fields = ('id', 'publication_id', 'publication_id')
+    list_display = ["publication_id", "journal_id", "title", "publication_date"]
         
 class QuotesAdmin(ImportExportModelAdmin):
     resource_class = QuotesResource
+    list_display = ["publication_id", "publication_id"]
     
 class WritesResource(resources.ModelResource):
     class Meta: 
