53cbbb9982c21214e11255b5eda3c8fce18fb53e
Mario Andrés Rosales Peña <mariorosales@ciencias.unam.mx>
2022-12-08 20:25:19 -0600

    Add publication page

--- scinet_app/urls.py
+++ scinet_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from scinet_app import views
 
 urlpatterns = [
-    path('main/', views.index),
+    path('main/', views.index, name='index'),
     path('main/register', views.register),
     path('publication/<int:publication_id>/', views.publication, name='publication'),
     path('user/<int:user_id>/', views.user, name='user'),
