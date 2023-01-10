a6a3f9be1b82c70bb0bad030fb6617d3f0fa98d7
xinwen-zhangliu <xinwen.zhangliu@ciencias.unam.mx>
2023-01-09 23:27:40 -0600

    fixed some tests

--- scinet_app/management/commands/test_populate.py
+++ scinet_app/management/commands/test_populate.py
@@ -1,6 +1,6 @@
 from django.core.management import call_command
 from django.test import TestCase
-from scinet_app.models import Publication
+from scinet_app.models import Publication, GeneralUser
 
 class PublicationsTestCase(TestCase):
     
@@ -13,39 +13,21 @@ class PublicationsTestCase(TestCase):
         #see if all publications have authors
         publications = []
         publications = Publication.objects.all()
-        #size = Publication.objects.all().__len__()
-        print("Number of publications: "+str(publications.__len__())+"\n")
+
+        authors = []
+        authors = GeneralUser.objects.all()
+
+        for publication in publications:
+            for author in authors:
+                if author.publications.filter(publication_id=publication.publication_id).exists():
+                    self.assertTrue(True)
+                    break
+                else:
+                    print(str(publication.publication_id )+"\n")
+                    self.assertTrue(False)
+                    break
+
         
-        for pub in publications:
-            if pub.generaluser_set.filter(slug=requested_slug).exists():
-                self.assertTrue(True)
-            else:
-                self.assertTrue(False)
-                print(pub.publication_id +'\n')
 
         
          
-# class PublicationTestCase(TestCase):
-    
-#     def pub_have_authors(self):
-#         # publication = Publication.objects.get(publication_id=publication_id)
-#         # all_authors = GeneralUser.objects.all()
-#         # authors = []
-#         # for author in all_authors:
-#         #     if publication in author.publications.all():
-#         #         authors.append(author)
-#         unittest.run(populate)
-#         publications = []
-#         publications = Publication.objects.all()
-        
-#         for pub in publications:
-#             if pub.generaluser_set.filter(slug=requested_slug).exists():
-#                 self.assertTrue(False)
-
-#         #if user.partner_set.filter(slug=requested_slug).exists():
-#      # do some private stuff
-        
-#         # for pub in publications:
-#         #     pub.
-#         # self.assertTrue(publications)
-
